c1ce15a3c03c11e865dc55384a68075462c4027a
vparent <v.parent215@laposte.net>
2021-02-11 16:29:30 +0100

    Harden scripts, add a distinction between vm and nat mode

--- network_down.sh
+++ network_down.sh
@@ -4,20 +4,20 @@ inet_interf=wlp0s20f0u7
 dnsmask_cnf=/etc/dnsmasq-qemu.conf
 	
 dnsmasq_pid=$(ps aux | grep "dnsmasq --conf-file=$dnsmask_cnf$" | awk '{print $2}')
-sudo kill $dnsmasq_pid
+kill "$dnsmasq_pid"
 
-sudo systemctl restart iptables
+systemctl restart iptables
 
-sudo sysctl net.ipv4.ip_forward=0
+sysctl net.ipv4.ip_forward=0
 
-sudo ip link set dev tap0 down
-sudo ip link set br0 up down
+ip link set dev tap0 down
+ip link set br0 up down
 
-sudo ip address del 172.30.0.10/16 dev tap0
-sudo ip address del 172.30.0.1/16 dev br0
+ip address del 172.30.0.10/16 dev tap0
+ip address del 172.30.0.1/16 dev br0
 
-sudo ip tuntap del dev tap0 mode tap
-sudo ip link del br0 type bridge
+ip tuntap del dev tap0 mode tap
+ip link del br0 type bridge
 
-sudo rmmod tap
-sudo rmmod tun
+rmmod tap
+rmmod tun
--- network_up.sh
+++ network_up.sh
@@ -1,33 +1,45 @@
 #! /bin/bash
 
+## CONF
+
+DHCP_PORT=67
+DNS_PORT=53
+NET_IP=172.30.0.1
+
 inet_interf=wlo1
 dnsmask_cnf=/etc/dnsmasq-qemu.conf
+mode=nat
 
-# Load necessary kernel module for networking (the necessary modules are tun and tap,
-# tun is already loaded by default)
-sudo modprobe tun
-sudo modprobe tap
+## END_CONF
 
 # Network interface
-sudo ip link add br0 type bridge
-sudo ip tuntap add dev tap0 mode tap
-sudo ip link set dev tap0 master br0
-# sudo ip link set dev $inet_interf master br0
-sudo ip address add 172.30.0.1/16 dev br0
-sudo ip  address add 172.30.0.10/16 dev tap0
-sudo ip link set dev br0 up
-sudo ip link set dev tap0 up
+ip link add br0 type bridge
+ip link set dev tap0 master br0
+
+if [ "$mode" == nat ]; then
+	modprobe tap
+
+	ip tuntap add dev tap0 mode tap
+
+	ip address add 172.30.0.1/16 dev br0
+	ip  address add 172.30.0.10/16 dev tap0
+else
+	ip link set dev $inet_interf master br0
+fi
+
+ip link set dev br0 up
+ip link set dev tap0 up
 
 # Enable nat
-sudo sysctl net.ipv4.ip_forward=1
+sysctl net.ipv4.ip_forward=1
 
 # Firewall configuration
-sudo iptables -t nat -A POSTROUTING -o $inet_interf -j MASQUERADE
-sudo iptables -t filter -A FORWARD -i br0 -o $inet_interf -j ACCEPT
-sudo iptables -t filter -A FORWARD -i $inet_interf -o br0 -j ACCEPT
+iptables -t nat -A POSTROUTING -o "$inet_interf" -j MASQUERADE
+iptables -t filter -A FORWARD -i br0 -o "$inet_interf" -j ACCEPT
+iptables -t filter -A FORWARD -i "$inet_interf" -o br0 -j ACCEPT
 
 # DHCP configuration
-sudo iptables -t filter -A UDP -p udp -m udp --dport 67 -j ACCEPT
-sudo iptables -t filter -A UDP -p udp -m udp --dport 53 -j ACCEPT # Enable DNS port
-sudo iptables -t filter -A TCP -p tcp -m tcp --dport 53 -j ACCEPT
-sudo dnsmasq --conf-file=$dnsmask_cnf
+iptables -t filter -A UDP -p udp -m udp --dport "$DHCP_PORT"-j ACCEPT
+iptables -t filter -A UDP -p udp -m udp --dport "$DNS_PORT" -j ACCEPT # Enable DNS port
+iptables -t filter -A TCP -p tcp -m tcp --dport "$DNS_PORT" -j ACCEPT
+dnsmasq --conf-file="$dnsmask_cnf"
